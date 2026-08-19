d2ac8439d74dba68a9af1f31a77850fdb1a26dda
nrqu <30873095+nrqu@users.noreply.github.com>
2026-08-18 19:37:53 -0500

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        int prev = 0;
+        for(int i = 0 ; i < nums.length; ++i){
+            if(nums[i] != val){
+                nums[prev] = nums[i];
+                ++prev;
+                                ++count;
+
+            }else{
+            }
+        }
+        return count;
+    }
+}
+
+
