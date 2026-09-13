3bbb550dc998a57dce9dc3c48267b970d17b70eb
nrqu <30873095+nrqu@users.noreply.github.com>
2026-09-13 14:03:12 -0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length( ) > s2.length()) return false;
+        int[] s1count = new int[26];
+        int[] window = new int[26];
+        int windowSize = s1.length();
+        for(int i = 0 ; i < s1.length(); ++i) {
+            s1count[s1.charAt(i)-'a']++;
+        }
+        for(int i = 0; i < windowSize; ++i){
+            window[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(window,s1count)) return true;
+
+        for(int i = windowSize; i < s2.length();++i){
+            window[s2.charAt(i)-'a']++;
+            window[s2.charAt(i-windowSize)-'a']--;
+            if(Arrays.equals(window,s1count)) return true;
+        }
+        return false;
+    }
+}
